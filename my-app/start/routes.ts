cafb2497bd1505b5c4a912ff5777f6f3d4c6d11c
Alen Boric <boricalen@live.com>
2023-04-04 23:43:08 +0200

    View ,Post,Delete Routes , Params,Named Route

--- my-app/start/routes.ts
+++ my-app/start/routes.ts
@@ -9,4 +9,25 @@ Route.get('/', async ({ view }) => {
 //   return view.render("news.view")
 // })
 
-Route.on("/news").render("news.view")
+Route.on("/news").render("news.view").as('news_view')
+
+Route.post('/news',({response})=>{
+  //const {email,password} = request.body()
+  return response.redirect('/news')
+}).as('news_create')
+
+
+Route.patch('/news/:id',({params})=>{
+  // console.log(params)
+  return {params}
+}).where('id', {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+}).as('news_update')
+
+Route.delete('/news/:id',({params})=>{
+  return {params}
+}).where('id', {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+}).as('news_delete')
